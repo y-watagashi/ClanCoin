a2ba4e4980f89b04467e251e45083ef8e8609364
k-downey <kinchan.yamamoto@gmail.com>
2023-05-06 14:19:48 +0900

    [update]Add clancoin models

--- backend/clancoin/models.py
+++ backend/clancoin/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from accounts.models import UserAccount
 
-# Create your models here.
+# 親ユーザー
+class ParentUser(models.Model):
+    user = models.ForeignKey(UserAccount, on_delete=models.CASCADE)
+    address = models.CharField(max_length=100)
+
+# 子供ユーザー
+class ChildUser(models.Model):
+    user = models.ForeignKey(UserAccount, on_delete=models.CASCADE)
+    address = models.CharField(max_length=100)
+    balance = models.IntegerField()
+
+# 組織(どの親にどの子が所属しているか保持する)
+class Organization(models.Model):
+    parent_user = models.ForeignKey(ParentUser, on_delete=models.CASCADE)
+    child_user = models.ForeignKey(ChildUser, on_delete=models.CASCADE)
+
+# 取引履歴
+# 親から子への通貨の移動を保持する
+# 子が親へ通貨を支払った際は，負の数として保持する
+class TreatHistory(models.Model):
+    parent_user = models.ForeignKey(ParentUser, on_delete=models.CASCADE)
+    child_user = models.ForeignKey(ChildUser, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+    
