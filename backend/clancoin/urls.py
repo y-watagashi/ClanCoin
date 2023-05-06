bac94362c7d7adf5d60776a802d844a4297d3674
k-downey <kinchan.yamamoto@gmail.com>
2023-05-06 16:22:09 +0900

    [update]add user's CRUD.

--- backend/clancoin/urls.py
+++ backend/clancoin/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from . import views
+from .views import *
+from rest_framework import routers
 
-urlpatterns = [
-    path('auth-test/', views.LoginRequired.as_view()),
-    path('test/', views.test),
-]
+
+router = routers.DefaultRouter()
+router.register('users', UserViewSet)
+
+urlpatterns = router.urls
--- backend/clancoin/views.py
+++ backend/clancoin/views.py
@@ -1,9 +1,11 @@
 from django.http.response import JsonResponse
-from django.contrib.auth.decorators import login_required
 
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
 
+from .models import *
+from .serializers import *
 
 # Create your views here.
 def test(req):
@@ -14,3 +16,10 @@ class LoginRequired(APIView):
     permission_classes = [IsAuthenticated]
     def get(self, req):
         return JsonResponse({"message": "seccess"})
+
+# ユーザーに関する操作
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
