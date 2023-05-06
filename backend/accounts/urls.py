f9eccafb6d6b552faa2be3190dad4d01c1bf227a
k-downey <kinchan.yamamoto@gmail.com>
2023-05-06 09:47:25 +0900

    [fix]Delete cache all of file.

--- backend/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import UserList, activate_user
-
-urlpatterns = [
-    path('users/', UserList.as_view()),
-    path('activate/<str:uid>/<str:token>', activate_user),
-
-]
--- backend/clancoin/views.py
+++
@@ -1,5 +0,0 @@
-from django.http.response import JsonResponse
-
-# Create your views here.
-def test(req):
-    return JsonResponse({"message": "test"})
