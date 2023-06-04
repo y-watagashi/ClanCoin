6c43bd713ca3b57c56374d767b3fc24e26db9e77
y-watagashi <kinchan.yamamoto@gmail.com>
2023-06-04 14:40:25 +0900

    :fire: ログイン済みのレイアウトを追加

---
+++ frontend/layouts/loggedin.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-app>
+      <v-main>
+        <AppBar/>
+        <Nuxt />
+        <BottomNavi/>
+      </v-main>
+    </v-app>
+  </template>
+  
+  <script>
+  import AppBar from '/components/AppBar'
+  import BottomNavi from '@/components/BottomNavi'
+  export default {
+    components:{
+      AppBar,
+      BottomNavi,
+    },
+    data(){
+      return {
+      }
+    },
+  
+  }
+  </script>
