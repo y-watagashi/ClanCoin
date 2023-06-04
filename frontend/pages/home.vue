740c6e7ee32fe0462db37cdb624e1369b66c4080
y-watagashi <kinchan.yamamoto@gmail.com>
2023-06-04 14:43:42 +0900

    :wastebasket: 不要な要素の削除

--- frontend/pages/home.vue
+++ frontend/pages/home.vue
@@ -1,11 +1,10 @@
 <template>
   <div>
-    <p>this is a home page</p>
-    is_login: {{ is_login }} user: {{ user }}
-    <p></p>
-    <v-btn @click="logout">logout</v-btn>
-    <v-btn @click="fetchuser">fetch user</v-btn>
-    <QRCode/>
+    <v-row>
+      <v-col>
+        <QRCode class="mt-10" />
+      </v-col>
+    </v-row>
   </div>
 </template>
 <script>
@@ -33,14 +32,16 @@ export default {
     },
     async fetchuser() {
       const token = this.$auth.strategy.token.get()
-      axios.get('http://localhost:8000/api/auth/users/me/', {
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': token
-        }
-      }).then((res) => {
-        console.log(res.data)
-      });
+      axios
+        .get('http://localhost:8000/api/auth/users/me/', {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: token,
+          },
+        })
+        .then((res) => {
+          console.log(res.data)
+        })
     },
   },
 }
