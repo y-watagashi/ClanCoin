3e005ff92ce48a72a6bb2930ecc2e3b6cd3fff3b
y-watagashi <kinchan.yamamoto@gmail.com>
2023-06-04 14:22:17 +0900

    :sparkles: ボトムナビゲーションを追加

---
+++ frontend/components/BottomNavi.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-bottom-navigation absolute rounded v-model="value">
+      <v-btn value="recent">
+        <span>Recent</span>
+  
+        <v-icon>mdi-history</v-icon>
+      </v-btn>
+  
+      <v-btn value="favorites">
+        <span>Favorites</span>
+  
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+  
+      <v-btn value="nearby">
+        <span>Nearby</span>
+  
+        <v-icon>mdi-map-marker</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+  </template>
+  <script>
+  export default {
+    data: () => ({ value: 'recent' }),
+  }
+</script>
